1e542e0ae66ccee487d46129f14c6803ffe95a68
Vimukthi Weerasiri <vimukthiweerasiri@gmail.com>
2015-08-13 13:20:20 +0530

    added a help line

--- Matlab/start.m
+++ Matlab/start.m
@@ -1,5 +1,5 @@
 I = read_mha('Images/Input/brain1.mha');
 % getting the middle plane
 MIDDLE_PLANE = I(:,:,round(end/2));
-MP2GRAY_SCALE = mat2gray(MIDDLE_PLANE);
-imshow(MP2GRAY_SCALE);
+MP2GRAY = mat2gray(MIDDLE_PLANE);
+imshow(MP2GRAY);
