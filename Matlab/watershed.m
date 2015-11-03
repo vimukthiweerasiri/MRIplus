67268c5e1510ebea77504e76108af6a02afcc0cc
dinal24 <kdinal24@gmail.com>
2015-11-03 22:08:45 +0530

    created the function

---
+++ Matlab/watershed.m
@@ -0,0 +1,27 @@
+function Ifinal = watershed(I)
+    hy = fspecial('sobel');
+    hx = hy';
+    Iy = imfilter(double(I), hy, 'replicate');
+    Ix = imfilter(double(I), hx, 'replicate');
+    gradmag = sqrt(Ix.^2 + Iy.^2);
+    se = strel('disk', 20);
+    Io = imopen(I, se);
+    Ie = imerode(I, se);
+    Iobr = imreconstruct(Ie, I);
+    
+    Ioc = imclose(Io, se);
+    Iobrd = imdilate(Iobr, se);
+    Iobrcbr = imreconstruct(imcomplement(Iobrd), imcomplement(Iobr));
+    Iobrcbr = imcomplement(Iobrcbr);
+    fgm = imregionalmax(Iobrcbr);
+    I2 = I;
+    I2(fgm) = 255;
+    se2 = strel(ones(5,5));
+    fgm2 = imclose(fgm, se2);
+    fgm3 = imerode(fgm2, se2);
+    fgm4 = bwareaopen(fgm3, 20);
+    Ifinal = fgm4;   
+    %I3 = I;
+    %Ifinal(fgm4) = 255;
+    imshow(Ifinal);
+end
