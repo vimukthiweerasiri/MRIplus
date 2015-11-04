4ae25f48cdda7d9be0bbe24271447cfcf3082ca0
Vimukthi Weerasiri <vimukthiweerasiri@gmail.com>
2015-11-05 00:24:49 +0530

    some fixes in the method

--- Matlab/generate_image.m
+++ Matlab/generate_image.m
@@ -1,12 +1,13 @@
 function RESULT = generate_image(INPUT, N, NET)
   [HEIGHT, WIDTH] = size(INPUT);
+  INPUT = double(INPUT);
   NEW_IMAGE = INPUT;
 
   for i = 1:N:HEIGHT-N
     for j = 1:N:WIDTH-N
       SUB_INPUT = INPUT(i: i + N - 1, j: j + N - 1);
       NORMALIZED_MAT = normalize(SUB_INPUT);
-      RESULT = sim(NET, NORMALIZED_MAT);
+      RESULT = sim(NET, NORMALIZED_MAT(:));
       NEW_IMAGE(i: i + N - 1, j: j + N - 1) = (round(RESULT) * 255);
     end
   end
