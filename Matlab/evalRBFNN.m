bfd9d59a562c40609460a07eabd464a8950b2033
Vimukthi Weerasiri <vimukthiweerasiri@gmail.com>
2016-01-06 12:30:23 +0530

    change to leave out test

--- Matlab/evalRBFNN.m
+++ Matlab/evalRBFNN.m
@@ -1,30 +1,34 @@
-function [CM] = evalRBFNN(STARTING_POINT, SPAN, TEST_PERCENTAGE, GOAL, SPREAD_CONSTANT, MN, DF)
+function [CM] = evalRBFNN(STARTING_POINT, SPAN, TEST_START, TEST_SPAN, GOAL, SPREAD_CONSTANT, MN, DF)
 % this returns the confusion matrix for the given evalution
     ALL_INPUT = load('Data/DATA.mat', 'INPUT');
     ALL_TARGET = load('Data/DATA.mat', 'TARGET');
     ALL_INPUT = ALL_INPUT.INPUT;
     ALL_TARGET = ALL_TARGET.TARGET;
     LENGTH = length(ALL_TARGET);
-    if SPAN < 1
-        SPAN = LENGTH * SPAN;
-    end
     
     START = int16(LENGTH * STARTING_POINT);
     END = int16(START + SPAN);
-    TEST_START = int16(END - SPAN * TEST_PERCENTAGE);
+    TEST_START = START + TEST_START;
+    TEST_END = TEST_START + TEST_SPAN;
     
-    INPUT_TRAIN = ALL_INPUT(:, START:TEST_START);
-    TARGET_TRAIN = ALL_TARGET(START:TEST_START);
+    disp(START);
+    disp(TEST_START);
+    disp(TEST_END);
+    disp(END);
     
-    if nargin < 7
+    INPUT_TRAIN = horzcat(ALL_INPUT(:, START:TEST_START - 1), ALL_INPUT(:, TEST_END:END));
+    TARGET_TRAIN = horzcat(ALL_TARGET(START:TEST_START - 1), ALL_TARGET(TEST_END:END));
+    
+    
+    if nargin < 8
      DF = 25;
-        if nargin < 6
+        if nargin < 7
             MN = TEST_START - START + 1;
         end
     end
 
-    INPUT_TEST = ALL_INPUT(:, TEST_START + 1: END);
-    TARGET_TEST = ALL_TARGET(TEST_START + 1: END);
+    INPUT_TEST = ALL_INPUT(:, TEST_START: TEST_END - 1);
+    TARGET_TEST = ALL_TARGET(TEST_START: TEST_END - 1);
 
     NN = newrb(INPUT_TRAIN, TARGET_TRAIN, GOAL, SPREAD_CONSTANT, MN, DF);
     OUTPUT = sim(NN, INPUT_TEST);
