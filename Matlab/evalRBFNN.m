333503b2b3220d65c488332a5aa60e67ab7c53ec
Vimukthi Weerasiri <vimukthiweerasiri@gmail.com>
2016-01-06 13:15:50 +0530

    automation done

--- Matlab/evalRBFNN.m
+++ Matlab/evalRBFNN.m
@@ -1,4 +1,4 @@
-function [CM] = evalRBFNN(STARTING_POINT, SPAN, TEST_START, TEST_SPAN, GOAL, SPREAD_CONSTANT, MN, DF)
+function [CM] = evalRBFNN(STARTING_POINT, SPAN, TEST__START, TEST_SPAN, GOAL, SPREAD_CONSTANT, MN, DF)
 % this returns the confusion matrix for the given evalution
     ALL_INPUT = load('Data/DATA.mat', 'INPUT');
     ALL_TARGET = load('Data/DATA.mat', 'TARGET');
@@ -6,29 +6,35 @@ function [CM] = evalRBFNN(STARTING_POINT, SPAN, TEST_START, TEST_SPAN, GOAL, SPR
     ALL_TARGET = ALL_TARGET.TARGET;
     LENGTH = length(ALL_TARGET);
     
+    disp(TEST__START);
+    disp(TEST_SPAN);
+    
     START = int16(LENGTH * STARTING_POINT);
     END = int16(START + SPAN);
-    TEST_START = START + TEST_START;
+    TEST_START = START + TEST__START;
     TEST_END = TEST_START + TEST_SPAN;
     
-    disp(START);
     disp(TEST_START);
     disp(TEST_END);
-    disp(END);
     
     INPUT_TRAIN = horzcat(ALL_INPUT(:, START:TEST_START - 1), ALL_INPUT(:, TEST_END:END));
     TARGET_TRAIN = horzcat(ALL_TARGET(START:TEST_START - 1), ALL_TARGET(TEST_END:END));
+    disp(size(INPUT_TRAIN));
+    disp(size(TARGET_TRAIN));
     
     
     if nargin < 8
      DF = 25;
         if nargin < 7
-            MN = TEST_START - START + 1;
+            MN = length(TARGET_TRAIN);
         end
     end
 
     INPUT_TEST = ALL_INPUT(:, TEST_START: TEST_END - 1);
     TARGET_TEST = ALL_TARGET(TEST_START: TEST_END - 1);
+    
+    disp(size(INPUT_TEST));
+    disp(size(TARGET_TEST));
 
     NN = newrb(INPUT_TRAIN, TARGET_TRAIN, GOAL, SPREAD_CONSTANT, MN, DF);
     OUTPUT = sim(NN, INPUT_TEST);
--- Matlab/sweetspot.m
+++ Matlab/sweetspot.m
@@ -1,20 +1,21 @@
 function sweetspot()
     fid = fopen('results.txt', 'a+');
     start = 0.5;
-    span = 2000;
-    testPercentage = 0.5;
+    span = 1000;
+    testSpan = 100;
     error = 5e-5;
-    spread = 1;
+    spread = 150;
     
     % change these when need and append to the arguments
     mn = 0;
     df = 0;
     
-    for SPD = 1: 50: 500
-        spread = SPD;
-        P = evalRBFNN(start, span, testPercentage, error, spread);
-        fprintf(fid, 'start: %d, span: %d, testPercentage: %f, error: %f, spread: %f\t||\tTP: %d, TN: %d, FP: %d, FN: %d\n', start, span, testPercentage, error, spread, P(4), P(1), P(2), P(3));
+    
+    for TSPAN = 1 : testSpan : span - testSpan
+        P = evalRBFNN(start, span, TSPAN, testSpan - 1, error, spread);
+        fprintf(fid, 'start: %d, span: %d, testStart: %d, testEnd: %d, error: %f, spread: %f\t||\tTP: %d, TN: %d, FP: %d, FN: %d\n', start, span, TSPAN, TSPAN + testSpan - 1, error, spread, P(4), P(1), P(2), P(3));
     end
+    
     fclose(fid);
 
 end
