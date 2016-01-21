d728926c98baad16d493620f1f678022e94b0f3c
dinal24 <kdinal24@gmail.com>
2016-01-21 17:12:29 +0530

    Added edges of tumors, related scripts & data

--- Matlab/Data/generate_allData_once.m
+++ Matlab/Data/generate_allData_once.m
@@ -1,13 +1,22 @@
-function generate_allData_once( TILE_SIZE )
+function generate_allData_once( TILE_SIZE, WITH_EDGES, EDGE_LABEL)
 % This function generate the data for ANN
 % TILE_SIZE: Breaks images into n x n tiles. Default is 8
     if nargin < 1
         TILE_SIZE = 8;
     end
+    
+    if nargin < 2
+        WITH_EDGES = false;
+    end
+    
+    if nargin < 3
+        EDGE_LABEL = -1;
+    end    
+        
 
-    INPUT32 = imread('../Images/SET1/MIDDLE_PLANES/IN/i32.png');
-    TARGET32 = imread('../Images/SET1/MIDDLE_PLANES/OUT/o32.png');
-    [INPUT TARGET] = tilize(INPUT32, TARGET32, TILE_SIZE);
+    INPUT32 = imread('../Images/SET1/MIDDLE_PLANES/IN/i38.png');
+    TARGET32 = imread('../Images/SET1/MIDDLE_PLANES/OUT/o38.png');
+    [INPUT TARGET] = tilize(INPUT32, TARGET32, TILE_SIZE, WITH_EDGES);
     
         
 	for i = 33:80
@@ -22,33 +31,52 @@ function generate_allData_once( TILE_SIZE )
     TARGET_ONE = [];
     INPUT_ZERO = [];
     TARGET_ZERO = [];
-    for index = 1:length(INPUT)
+    
+    % MONE - MINUS ONE
+    INPUT_MONE = [];     
+    TARGET_MONE = [];
+    
+    for index = 1:length(TARGET)
         if TARGET(index) == 1
             INPUT_ONE = [INPUT_ONE INPUT(:, index)];
             TARGET_ONE = [TARGET_ONE TARGET(index)];
-        else
+        elseif TARGET(index) == 0
             INPUT_ZERO = [INPUT_ZERO INPUT(:, index)];
             TARGET_ZERO = [TARGET_ZERO TARGET(index)];
+        elseif WITH_EDGES
+            INPUT_MONE = [INPUT_MONE INPUT(:, index)];
+            TARGET_MONE = [TARGET_MONE EDGE_LABEL];
         end
     end
     
-    rIdx = randperm(length(INPUT_ZERO));
+    % Debugging       
+    disp(size(INPUT_ONE))
+    disp(size(INPUT_ZERO))
+    disp(size(INPUT_MONE))
+    
+    rIdx = randperm(length(TARGET_ZERO));
     rZERO_INPUT = INPUT_ZERO(:, rIdx);
     rZERO_TARGET = TARGET_ZERO(rIdx);
     
-    rZERO_INPUT_FIRST = rZERO_INPUT(:, 1:length(TARGET_ONE));
-    rZERO_TARGET_FIRST = rZERO_TARGET(1:length(TARGET_ONE));
+    len = max(length(TARGET_ONE),length(TARGET_MONE));
+    
+    rZERO_INPUT_FIRST = rZERO_INPUT(:, 1:len);
+    rZERO_TARGET_FIRST = rZERO_TARGET(1:len);
     
     fINPUT = [INPUT_ONE rZERO_INPUT_FIRST];
     fTARGET = [TARGET_ONE rZERO_TARGET_FIRST];
+    
+    if WITH_EDGES
+        fINPUT = [fINPUT INPUT_MONE];
+        fTARGET = [fTARGET TARGET_MONE];
     
     rFIdx = randperm(length(fTARGET));
     equalizedINPUT = fINPUT(:, rFIdx);
     equalizedTARGET = fTARGET(rFIdx);
     
-    save('equalizedDATA', 'equalizedINPUT', 'equalizedTARGET');
+%    save('equalizedDATA', 'equalizedINPUT', 'equalizedTARGET');
+    save('Edge_equalizedDATA8x8', 'equalizedINPUT', 'equalizedTARGET');
     
-
 %     nrINPUT = INPUT;
 %     nrTARGET = TARGET;
     %save('notRandomizedDATA', 'nrINPUT', 'nrTARGET');
--- Matlab/tilize.m
+++ Matlab/tilize.m
@@ -1,45 +1,110 @@
-function [T_INPUT, T_OUTPUT] = tilize(INPUT, OUTPUT, N)
-  INPUT = double(INPUT);
-  OUTPUT = double(OUTPUT);
-  [HEIGHT, WIDTH] = size(INPUT);
-  TRAINING_IDX = 1;
-  TRAINING_OUTPUT = [];
-  TRAINING_INPUT = [];
-
-  for i = 1:N:HEIGHT-N
-    for j = 1:N:WIDTH-N
-      SUB_INPUT = INPUT(i: i + N - 1, j: j + N - 1);
-      SUB_OUTPUT = OUTPUT(i: i + N - 1, j: j + N - 1);
-      IN_SUM = sum(SUB_INPUT(:) == 0);
-      if IN_SUM == N*N
-          continue;
-      end
-      OUT_SUM = sum(SUB_OUTPUT(:) == 0);
-      if OUT_SUM == 0
-%         disp('YES');
-        TRAINING_OUTPUT(TRAINING_IDX) = 1;
-
-        NORM_RESULT = normalize(SUB_INPUT);
-%         disp(NORM_RESULT);
-        TRAINING_INPUT(:,TRAINING_IDX) = NORM_RESULT(:);
-
-        TRAINING_IDX = TRAINING_IDX + 1;
-      end
-      if OUT_SUM == N*N
-%         disp('NO');
-        TRAINING_OUTPUT(TRAINING_IDX) = 0;
-
-        NORM_RESULT = normalize(SUB_INPUT);
-%         disp(NORM_RESULT);
-        TRAINING_INPUT(:,TRAINING_IDX) = NORM_RESULT(:);
-
-        TRAINING_IDX = TRAINING_IDX + 1;
-      end
+function [T_INPUT, T_OUTPUT] = tilize(INPUT, OUTPUT, N, WITH_EDGES)
+    if (nargin < 4)
+        WITH_EDGES = false;
     end
-  end
-  T_INPUT = TRAINING_INPUT;
-  T_OUTPUT = TRAINING_OUTPUT;
+    INPUT = double(INPUT);
+    OUTPUT = double(OUTPUT);
+    [HEIGHT, WIDTH] = size(INPUT);
+    TRAINING_IDX = 1;
+    TRAINING_OUTPUT = [];
+    TRAINING_INPUT = [];
+
+    for i = 1:N:HEIGHT-N
+        for j = 1:N:WIDTH-N
+            SUB_INPUT = INPUT(i: i + N - 1, j: j + N - 1);
+            SUB_OUTPUT = OUTPUT(i: i + N - 1, j: j + N - 1);
+            IN_SUM = sum(SUB_INPUT(:) == 0);
+            if IN_SUM == N*N
+                continue;
+            end
+            
+            % Omits the edges of the brain
+            % Can confuse NN with tumor-brain edge
+            if WITH_EDGES
+                IS_EDGE = IsBrainEdge(INPUT, N, i, j, HEIGHT, WIDTH);
+                if IS_EDGE
+                    continue
+                end
+            end
+            OUT_SUM = sum(SUB_OUTPUT(:) == 0);
+            if OUT_SUM == 0
+                %         disp('YES');
+                TRAINING_OUTPUT(TRAINING_IDX) = 1;
+
+                NORM_RESULT = normalize(SUB_INPUT);
+                %         disp(NORM_RESULT);
+                TRAINING_INPUT(:,TRAINING_IDX) = NORM_RESULT(:);
+
+                TRAINING_IDX = TRAINING_IDX + 1;
+            elseif OUT_SUM == N*N
+                %         disp('NO');
+                TRAINING_OUTPUT(TRAINING_IDX) = 0;
+
+                NORM_RESULT = normalize(SUB_INPUT);
+                %         disp(NORM_RESULT);
+                TRAINING_INPUT(:,TRAINING_IDX) = NORM_RESULT(:);
+
+                TRAINING_IDX = TRAINING_IDX + 1;
+            elseif WITH_EDGES
+                %         disp('NO');
+                TRAINING_OUTPUT(TRAINING_IDX) = -1;
+
+                NORM_RESULT = normalize(SUB_INPUT);
+                %         disp(NORM_RESULT);
+                TRAINING_INPUT(:,TRAINING_IDX) = NORM_RESULT(:);
+
+                TRAINING_IDX = TRAINING_IDX + 1;
+            end
+        end
+    end
+    T_INPUT = TRAINING_INPUT;
+    T_OUTPUT = TRAINING_OUTPUT;
 end
+
+function IS_EDGE = IsBrainEdge(INPUT, N, i_c, j_c, H, W)
+% This functin checks if a given block contains edge of the brain
+% returns true for the edges, false otherwise
+    if (i_c - N) < 1
+        i_start = i_c;
+    else
+        i_start = i_c - N;
+    end
+    
+    if (i_c + N) > (H - N)
+        i_end = i_c;
+    else
+        i_end = i_c + N;
+    end
+    
+    if (j_c - N) < 1
+        j_start = j_c;
+    else
+        j_start = j_c - N;
+    end
+    
+    if (j_c + N) > (W - N)
+        j_end = j_c;
+    else 
+        j_end = j_c + N;
+    end
+    
+    TEMP = false;
+    for i = i_start:N:i_end
+        for j = j_start:N:j_end 
+            SUB_INPUT = INPUT(i: i + N - 1, j: j + N - 1);
+            SUB_SUM = sum(SUB_INPUT(:) == 0);
+            if SUB_SUM == N*N
+                TEMP = true;
+                break;
+            end
+        end
+        if TEMP
+            break;
+        end
+    end
+   IS_EDGE = TEMP;
+end
+
 
 function MAT = normalize(INPUT)
     VEC_MAT = INPUT(:);
@@ -47,7 +112,7 @@ function MAT = normalize(INPUT)
     VAR = var(double(VEC_MAT), 1);
     INPUT = INPUT - MEAN;
     if VAR ~= 0
-      INPUT = INPUT / VAR;
+        INPUT = INPUT / VAR;
     end
-  MAT = INPUT;
+    MAT = INPUT;
 end
