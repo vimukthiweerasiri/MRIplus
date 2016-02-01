e6f33f41d3272b3251b6b621dcd6051b199eeb04
Vimukthi Weerasiri <vimukthiweerasiri@gmail.com>
2016-02-01 19:20:29 +0530

    Message passing work..lol

---
+++ App/client/startup.js
@@ -0,0 +1,10 @@
+/**
+ * Created by vimukthi on 2/1/16.
+ */
+Meteor.startup(function() {
+    console.log("client started");
+    var dicom = "dfasdfsd";
+    Meteor.call('predict', dicom, function(err, data){
+        console.log(err, data);
+    })
+});
---
+++ App/server/methods.js
@@ -0,0 +1,8 @@
+/**
+ * Created by vimukthi on 2/1/16.
+ */
+Meteor.methods({
+    predict: function (base64string) {
+        return "vimukthi";
+    }
+});
