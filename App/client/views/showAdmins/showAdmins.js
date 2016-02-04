b2eeeab3436c6f44ac56abc6ea8daa1221e2543e
Vimukthi Weerasiri <vimukthiweerasiri@gmail.com>
2016-02-04 21:34:02 +0530

    table views and nav bar

--- App/client/views/showAdmins/showAdmins.js
+++ App/client/views/showAdmins/showAdmins.js
@@ -2,7 +2,7 @@
  * Created by vimukthi on 2/4/16.
  */
 Template.showAdmins.events({
-    "click .makeAnAdmin": function (event, target) {
+    "click #makeAnAdmin": function (event, target) {
         Meteor.call('upgradeToAdmin', Meteor.userId(), this._id, function (err, data) {
             console.log(err, data);
         })
--- App/client/views/showInvalidates/showInvalidates.js
+++ App/client/views/showInvalidates/showInvalidates.js
@@ -2,7 +2,7 @@
  * Created by vimukthi on 2/4/16.
  */
 Template.showInvalidates.events({
-    "click .validateThisUser": function (event, target) {
+    "click #validateThisUser": function (event, target) {
         Meteor.call('upgradeToValidated', Meteor.userId(), this._id, function (err, data) {
             console.log(err, data);
         })
